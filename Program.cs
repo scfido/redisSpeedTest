06a441f5c8e8e1af4fd05d2f14ce640e2a423cb9
fuyun <scfido@gmail.com>
2017-09-20 13:59:56 +0800

    添加读取测试用例

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using StackExchange.Redis;
 
 namespace redisSpeedTest
@@ -14,11 +15,19 @@ namespace redisSpeedTest
             var db = redis.GetDatabase();
             var count = 10000;
             var ts = SingleSet(db, count);
-            Console.WriteLine($"单条写入{count}条耗时：{ts}");
-            Console.WriteLine();
+            Console.WriteLine($"\t单条写入{count}条耗时：{ts}");
 
             ts = BatchSet(db, count);
-            Console.WriteLine($"批量写入{count}条耗时：{ts}");
+            Console.WriteLine($"\t批量写入{count}条耗时：{ts}");
+            Console.WriteLine();
+
+            Console.WriteLine("Redis 读取性能测试");
+
+            ts = SingleGet(db, count);
+            Console.WriteLine($"\t单条读取{count}条耗时：{ts}");
+
+            ts = BatchGet(db, count);
+            Console.WriteLine($"\t批量读取{count}条耗时：{ts}");
         }
 
         static TimeSpan SingleSet(IDatabase db, int count)
@@ -28,9 +37,9 @@ namespace redisSpeedTest
             {
                 db.StringSet($"foo{i}", $"bar-{i}");
             }
-            Console.WriteLine($"foo{count - 1}={{0}}", db.StringGet($"foo{count - 1}"));
-
             sw.Stop();
+            //Console.WriteLine($"foo{count - 1}={{0}}", db.StringGet($"foo{count - 1}"));
+
             return sw.Elapsed;
         }
 
@@ -53,9 +62,55 @@ namespace redisSpeedTest
                 offset += size;
                 //Console.WriteLine($"写入{offset}/{count}");
             }
-            Console.WriteLine($"batchfoo{count - 1}={{0}}", db.StringGet($"foo{count - 1}"));
+            sw.Stop();
+
+            //Console.WriteLine($"batchfoo{count - 1}={{0}}", db.StringGet($"foo{count - 1}"));
+
+            return sw.Elapsed;
+        }
+
+        static TimeSpan SingleGet(IDatabase db, int count)
+        {
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < count; i++)
+            {
+                string value = db.StringGet($"foo{i}");
+            }
+            //Console.WriteLine($"foo{count - 1}={{0}}", db.StringGet($"foo{count - 1}"));
 
             sw.Stop();
+            return sw.Elapsed;
+        }
+
+        static TimeSpan BatchGet(IDatabase db, int count)
+        {
+            var batch = db.CreateBatch();
+
+            var sw = Stopwatch.StartNew();
+
+            int offset = 0;
+            int block = 100;
+            while (offset < count)
+            {
+                List<Task<RedisValue>> valueList = new List<Task<RedisValue>>();
+
+                int size = count - offset >= block ? block : count - offset;
+                for (int i = 0; i < size; i++)
+                {
+                    string key = $"batchfoo{offset + i}";
+                    Task<RedisValue> tres = batch.StringGetAsync(key);
+                    valueList.Add(tres);
+                }
+                batch.Execute();
+
+                foreach (var val in valueList)
+                {
+                    var value = val.Result.ToString();
+                }
+                offset += size;
+            }
+            sw.Stop();
+
             return sw.Elapsed;
         }
     }
